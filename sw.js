0cfd3354626f988629d4e9ad754c24bcf47ce613
jyunz47 <jyunz0447@gmail.com>
2026-07-03 21:35:23 +0800

    Add files via upload
    改掉iPhone瀏海的問題

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ep40-v1";
+const CACHE = "ep40-v2";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
